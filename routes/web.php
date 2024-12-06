0f59b47f379c82583a46394dd4695128d3b59172
FAirpLay <bcozfair@gmail.com>
2024-12-07 00:53:28 +0700

    Quiz Week 3

--- routes/web.php
+++ routes/web.php
@@ -81,3 +81,67 @@ Route::get("/gallery/cat", function () {
     $cat = "http://www.onyxtruth.com/wp-content/uploads/2017/06/black-panther-movie-onyx-truth.jpg";
     return view("test/cat", compact("cat"));
 });
+
+// Week3
+Route::get("/teacher", function () {
+    return view("teacher");
+});
+
+Route::get("/student", function () {
+    return view("student");
+});
+
+Route::get("/theme", function () {
+    return view("theme");
+});
+
+// Inheritance
+Route::get("/teacher/inheritance", function () {
+    return view("teacher-inheritance");
+});
+
+Route::get("/student/inheritance", function () {
+    return view("student-inheritance");
+});
+
+// Component
+Route::get("/teacher", function () {
+    return view("teacher");
+});
+
+Route::get("/student", function () {
+    return view("student");
+});
+
+// Week3 Quiz1
+Route::get('/active/index', function () {
+    return view('active/index');
+})->name ('index');
+
+// Week3 Quiz2
+Route::get('/active/about', function () {
+    return view('active/about');
+})->name('about');
+Route::get('/active/services', function () {
+    return view('active/services');
+})->name('services');
+Route::get('/active/portfolio', function () {
+    return view('active/portfolio');
+})->name('portfolio');
+Route::get('/active/portfolio-details', function () {
+    return view('active/portfolio-details');
+})->name('portfolio-details');
+Route::get('/active/team', function () {
+    return view('active/team');
+})->name('team');
+Route::get('/active/blog', function () {
+    return view('active/blog');
+})->name('blog');
+Route::get('/active/blog-details', function () {
+    return view('active/blog-details');
+})->name('blog-details');
+Route::get('/active/contact', function () {
+    return view('active/contact');
+})->name('contact');
+
+// {{ asset('asset jpg')}}
